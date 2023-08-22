0663ae0d863f2e573be06f627fc4cf8a0e796840
Dale G <dale.geronimo1@gmail.com>
2023-08-22 17:13:17 +1200

    Added MenuItems and put the GUI in a class

--- Rachel_Bell-20019755-Dale_Geronimo-22004533/Assignment1_159251_2023/src/main/java/org/example/Main.java
+++ Rachel_Bell-20019755-Dale_Geronimo-22004533/Assignment1_159251_2023/src/main/java/org/example/Main.java
@@ -5,43 +5,6 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Text Editor");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500, 400); // Set the size of the frame
-
-        // Create a panel to hold components
-        JPanel panel = new JPanel();
-        panel.setLayout(new BorderLayout());
-
-        // Create a menu bar
-        JMenuBar menuBar = new JMenuBar();
-
-        // Create menu items
-        JMenu fileMenu = new JMenu("File");
-        JMenu searchMenu = new JMenu("Search");
-        JMenu viewMenu = new JMenu("View");
-        JMenu manageMenu = new JMenu("Manage");
-        JMenu helpMenu = new JMenu("Help");
-
-        // Add menu items to the menu bar
-        menuBar.add(fileMenu);
-        menuBar.add(searchMenu);
-        menuBar.add(viewMenu);
-        menuBar.add(manageMenu);
-        menuBar.add(helpMenu);
-
-        // Set the menu bar to the frame
-        frame.setJMenuBar(menuBar);
-
-        // Create a JTextArea where the user can write things
-        JTextArea textArea = new JTextArea(10, 40);
-        JScrollPane scrollPane = new JScrollPane(textArea);
-        panel.add(scrollPane, BorderLayout.CENTER);
-
-        // Add the panel to the frame
-        frame.getContentPane().add(panel);
-
-        // Display the frame
-        frame.setVisible(true);
+        TextEditor textGUI = new TextEditor();
     }
 }
